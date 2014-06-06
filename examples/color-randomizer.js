c561c7c0086dc7a65c3aa58be7a8e1ce2fa47384
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-06 18:16:40 +1000

    Refactor examples to use a cleaner api

--- examples/color-randomizer.js
+++ examples/color-randomizer.js
@@ -1,36 +1,26 @@
-var skyportal = require('skyportal');
-var commands = require('skyportal/commands');
 var portal = require('../');
 var pull = require('pull-stream');
 
-// open the portal (may require admin privileges)
-skyportal.open(skyportal.find(), function(err, p) {
-  if (err) {
-    return console.error(err);
-  }
+pull(
+  // generate random rgb colors
+  pull.infinite(function() {
+    var r = (Math.random() * 255) | 0;
+    var g = (Math.random() * 255) | 0;
+    var b = (Math.random() * 255) | 0;
 
-  pull(
-    // generate random rgb colors
-    pull.infinite(function() {
-      var r = (Math.random() * 255) | 0;
-      var g = (Math.random() * 255) | 0;
-      var b = (Math.random() * 255) | 0;
+    return [r, g, b];
+  }),
 
-      return [r, g, b];
-    }),
+  // convert to a color command
+  pull.map(portal.commands.color),
 
-    // convert to a color command
-    pull.map(commands.color),
+  // throttle updates to prevent your eyes hurting
+  pull.asyncMap(function(data, cb) {
+    setTimeout(function() {
+      cb(null, data);
+    }, 100);
+  }),
 
-    // throttle updates to prevent your eyes hurting
-    pull.asyncMap(function(data, cb) {
-      setTimeout(function() {
-        cb(null, data);
-      }, 100);
-    }),
-
-    // send the data
-    portal.writer(p)
-  );
-});
-
+  // send the data
+  portal.writer()
+);
--- examples/stream-status.js
+++ examples/stream-status.js
@@ -1,17 +1,8 @@
-var skyportal = require('skyportal');
 var portal = require('../');
 var pull = require('pull-stream');
 
-// open the portal (may require admin privileges)
-skyportal.open(skyportal.find(), function(err, p) {
-  if (err) {
-    return console.error(err);
-  }
-
-  // read a stream of status updates from the portal
-  pull(
-    portal.reader(p),
-    pull.drain(console.log)
-  );
-});
-
+// read a stream of status updates from the portal
+pull(
+  portal.read(),
+  pull.drain(console.log)
+);

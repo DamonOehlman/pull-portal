ac6015dbc6fa0d0b5c75e25c1a196f8ba3c66d79
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-07 00:25:08 +1000

    Successfully capturing tag state change

--- tags.js
+++ tags.js
@@ -4,18 +4,46 @@ var pull = require('pull-core');
 module.exports = function() {
   var tags = new EventEmitter();
 
+  function findActiveTags(tagStat, currentState) {
+    var newState = [];
+    var tagIdx = 0;
+
+    while (tagStat > 0) {
+      newState[tagIdx] = tagStat & 1 == 1;
+      if (currentState && newState[tagIdx] != currentState[tagIdx]) {
+        console.log('tag ' + tagIdx + ' changed');
+      }
+
+      tagStat = tagStat >> 2;
+      tagIdx += 1;
+    }
+
+    while (newState.length < 12) {
+      newState.push(0);
+    }
+
+    return newState;
+  }
+
   function monitor(read) {
+    var active = findActiveTags(0);
+    var lastTagStat;
+
     return function(end, cb) {
       if (end) {
         return cb(end);
       }
 
       read(null, function(end, data) {
-        var flags;
+        var tagStat;
 
         if ((! end) && (data[0] == 0x53)) {
-          flags = (data[3] << 16) + (data[2] << 8) + data[1];
-          console.log(flags.toString(2), data);
+          tagStat = (data[3] << 16) + (data[2] << 8) + data[1];
+          if (tagStat !== lastTagStat) {
+            active = findActiveTags(tagStat, active);
+          }
+
+          lastTagStat = tagStat;
         }
 
         cb(end, data);
